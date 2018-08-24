677dbf86520ecb5b19ff15d1a3c28c84fbe006ad
Aona Suzutsuki <aonasuzutsuki@gmail.com>
2018-08-24 15:55:42 +0900

    added test codes.

---
+++ KeyConverter/KeyConverterGUI/InterceptInput.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace KeyConverterGUI
+{
+    public class InterceptInput
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public int dwExtraInfo;
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct KEYBDINPUT
+        {
+            public short wVk;
+            public short wScan;
+            public int dwFlags;
+            public int time;
+            public int dwExtraInfo;
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HARDWAREINPUT
+        {
+            public int uMsg;
+            public short wParamL;
+            public short wParamH;
+        };
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct INPUT
+        {
+            [FieldOffset(0)]
+            public int type;
+            [FieldOffset(4)]
+            public MOUSEINPUT no;
+            [FieldOffset(4)]
+            public KEYBDINPUT ki;
+            [FieldOffset(4)]
+            public HARDWAREINPUT hi;
+        };
+
+        [DllImport("user32.dll")]
+        private extern static void SendInput(int nInputs, ref INPUT pInputs, int cbsize);
+        [DllImport("user32.dll", EntryPoint = "MapVirtualKeyA")]
+        private extern static int MapVirtualKey(int wCode, int wMapType);
+
+        private const int INPUT_KEYBOARD = 1;
+        private const int KEYEVENTF_KEYDOWN = 0x0;
+        private const int KEYEVENTF_KEYUP = 0x2;
+        private const int KEYEVENTF_EXTENDEDKEY = 0x1;
+
+        public void Send(int key, bool isEXTEND)
+        {
+            /*
+             * Keyを送る
+             * 入力
+             *     isEXTEND : 拡張キーかどうか
+             */
+
+            INPUT inp = new INPUT();
+
+            // 押す
+            inp.type = INPUT_KEYBOARD;
+            inp.ki.wVk = (short)key;
+            inp.ki.wScan = (short)MapVirtualKey(inp.ki.wVk, 0);
+            inp.ki.dwFlags = ((isEXTEND) ? (KEYEVENTF_EXTENDEDKEY) : 0x0) | KEYEVENTF_KEYDOWN;
+            inp.ki.time = 0;
+            inp.ki.dwExtraInfo = 102;
+            SendInput(1, ref inp, Marshal.SizeOf(inp));
+
+            System.Threading.Thread.Sleep(100);
+
+            // 離す
+            inp.ki.dwFlags = ((isEXTEND) ? (KEYEVENTF_EXTENDEDKEY) : 0x0) | KEYEVENTF_KEYUP;
+            SendInput(1, ref inp, Marshal.SizeOf(inp));
+        }
+
+        public INPUT KeyDown(int key)
+        {
+            INPUT inp = new INPUT();
+
+            // 押す
+            inp.type = INPUT_KEYBOARD;
+            inp.ki.wVk = (short)key;
+            inp.ki.wScan = (short)MapVirtualKey(inp.ki.wVk, 0);
+            inp.ki.dwFlags = ((false) ? (KEYEVENTF_EXTENDEDKEY) : 0x0) | KEYEVENTF_KEYDOWN;
+            inp.ki.time = 0;
+            inp.ki.dwExtraInfo = 102;
+            SendInput(1, ref inp, Marshal.SizeOf(inp));
+            return inp;
+        }
+
+        public void KeyUp(INPUT input)
+        {
+            input.ki.dwFlags = ((false) ? (KEYEVENTF_EXTENDEDKEY) : 0x0) | KEYEVENTF_KEYUP;
+            SendInput(1, ref input, Marshal.SizeOf(input));
+        }
+    }
+}
---
+++ KeyConverter/KeyConverterGUI/InterceptKeys.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using static KeyConverterGUI.InterceptInput;
+
+namespace KeyConverterGUI
+{
+    class InterceptKeys : IDisposable
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+        private const int WM_SYSKEYDOWN = 0x0104;
+        private const int WM_SYSKEYUP = 0x0105;
+        private LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static InterceptInput input = new InterceptInput();
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class KBDLLHOOKSTRUCT
+        {
+            public uint vkCode;
+            public uint scanCode;
+            public KBDLLHOOKSTRUCTFlags flags;
+            public uint time;
+            public UIntPtr dwExtraInfo;
+        }
+        [Flags]
+        public enum KBDLLHOOKSTRUCTFlags : uint
+        {
+            LLKHF_EXTENDED = 0x01,
+            LLKHF_INJECTED = 0x10,
+            LLKHF_ALTDOWN = 0x20,
+            LLKHF_UP = 0x80,
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+        
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        public InterceptKeys()
+        {
+            _hookID = SetHook(_proc);
+        }
+        
+        private IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            {
+                using (ProcessModule curModule = curProcess.MainModule)
+                {
+                    return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+                }
+            }
+
+        }
+
+        private static INPUT inkey;
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN))
+            {
+                //int vkCode = Marshal.ReadInt32(lParam);
+                KBDLLHOOKSTRUCT kb = (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(KBDLLHOOKSTRUCT));
+                var vkCode = (int)kb.vkCode;
+                var key = KeyConverter.KeyCodeToKey(vkCode);
+                //if (key.Equals(Key.LeftAlt))
+                //{
+                if (kb.dwExtraInfo.ToUInt32() != 102u)
+                {
+                    if (key.Equals(Key.LeftAlt))
+                    {
+                        var inputKey = KeyConverter.KeyToCode(Key.LeftCtrl);
+                        inkey = input.KeyDown(inputKey);
+                        return new IntPtr(1);
+                    }
+                    else if (key.Equals(Key.LeftCtrl))
+                    {
+                        var inputKey = KeyConverter.KeyToCode(Key.LeftAlt);
+                        inkey = input.KeyDown(inputKey);
+                        return new IntPtr(1);
+                    }
+                }
+                else
+                {
+                    return CallNextHookEx(_hookID, nCode, wParam, lParam);
+                }
+            }
+            else if (nCode >= 0 && (wParam == (IntPtr)WM_KEYUP || wParam == (IntPtr)WM_SYSKEYUP))
+            {
+                input.KeyUp(inkey);
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+        
+
+        public void Dispose()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+    }
+}
