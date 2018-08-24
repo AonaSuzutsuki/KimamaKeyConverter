de7dac9ef845b6ff9293c2dfbb96969a45d388f5
Aona Suzutsuki <aonasuzutsuki@gmail.com>
2018-08-24 17:17:21 +0900

    Changed Copyright.

--- KeyConverter/KeyConverterGUI/Properties/AssemblyInfo.cs
+++ KeyConverter/KeyConverterGUI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KeyConverterGUI")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Aona Suzutsuki  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
