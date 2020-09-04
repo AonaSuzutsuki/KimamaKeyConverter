494332d47a8df248584e34c6e30e51774c697865
AonaSuzutsuki <rvvfake.org@gmail.com>
2020-09-04 23:07:44 +0900

    fixed focus

--- KeyConverter/KeyConverterGUI/Models/ProcessSettingModel.cs
+++ KeyConverter/KeyConverterGUI/Models/ProcessSettingModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Reactive.Subjects;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -14,27 +15,55 @@ using Prism.Mvvm;
 
 namespace KeyConverterGUI.Models
 {
+    public enum ProcessItemType
+    {
+        Dummy,
+        Item
+    }
+
     public class ProcessItemInfo : BindableBase
     {
         private string fullPath = string.Empty;
+        private bool isTextBoxFocus;
+
+
+        public ProcessItemType Type { get; set; } = ProcessItemType.Item;
 
         public string FullPath
         {
             get => fullPath;
-            set => SetProperty(ref fullPath, value);
+            set
+            {
+                SetProperty(ref fullPath, value);
+                fullPathChangedSubject.OnNext((this, value, Type));
+                Type = ProcessItemType.Item;
+            }
+        }
+
+        public bool IsTextBoxFocus
+        {
+            get => isTextBoxFocus;
+            set => SetProperty(ref isTextBoxFocus, value);
         }
 
         public ICommand SetFullPathCommand { get; set; }
+        public ICommand FullPathGotFocusCommand { get; set; }
 
         #region Event
 
-        
+        private readonly Subject<(ProcessItemInfo sender, string fullPath, ProcessItemType prevType)> fullPathChangedSubject
+            = new Subject<(ProcessItemInfo, string, ProcessItemType)>();
+        public IObservable<(ProcessItemInfo sender, string fullPath, ProcessItemType prevType)> FullPathChanged => fullPathChangedSubject;
 
+
+        private readonly Subject<ProcessItemInfo> fullPathGotFocuSubject = new Subject<ProcessItemInfo>();
+        public IObservable<ProcessItemInfo> FullPathGotFocus => fullPathGotFocuSubject;
         #endregion
 
         public ProcessItemInfo()
         {
             SetFullPathCommand = new DelegateCommand(SetFullPath);
+            FullPathGotFocusCommand = new DelegateCommand(FullPathFocus);
         }
 
         public void SetFullPath()
@@ -43,32 +72,31 @@ namespace KeyConverterGUI.Models
                 FileSelector.FileSelectorType.Read);
             FullPath = filePath ?? FullPath;
         }
+
+        public void FullPathFocus()
+        {
+            fullPathGotFocuSubject.OnNext(this);
+        }
     }
 
     public class ProcessSettingModel : ModelBase
     {
         #region Fields
 
-        private HashSet<string> processesSet = new HashSet<string>();
+        private readonly string jsonPath;
 
-        private ObservableCollection<ProcessItemInfo> processItems = new ObservableCollection<ProcessItemInfo>();
-        private string processesText;
-        private string jsonPath;
+        private ProcessItemInfo processSelectedItem;
 
         #endregion
 
         #region Properties
 
-        public ObservableCollection<ProcessItemInfo> ProcessItems
-        {
-            get => processItems;
-            set => SetProperty(ref processItems, value);
-        }
+        public ObservableCollection<ProcessItemInfo> ProcessItems { get; }
 
-        public string ProcessesText
+        public ProcessItemInfo ProcessSelectedItem
         {
-            get => processesText;
-            set => SetProperty(ref processesText, value);
+            get => processSelectedItem;
+            set => SetProperty(ref processSelectedItem, value);
         }
 
         #endregion
@@ -77,46 +105,64 @@ namespace KeyConverterGUI.Models
         {
             this.jsonPath = jsonPath;
 
-            ProcessItems = new ObservableCollection<ProcessItemInfo>
-            {
-                CreateProcessItemInfo(new ProcessItemInfo
-                {
-                    FullPath = "test"
-                }),
-                CreateProcessItemInfo(new ProcessItemInfo
-                {
-                    FullPath = "test2"
-                }),
-                CreateProcessItemInfo(new ProcessItemInfo
-                {
-                    FullPath = ""
-                })
-            };
-
             if (File.Exists(jsonPath))
             {
                 var json = File.ReadAllText(jsonPath);
-                processesSet = JsonConvert.DeserializeObject<HashSet<string>>(json);
-
-                ProcessesText = string.Join("\n", processesSet);
+                var processesSet = JsonConvert.DeserializeObject<HashSet<string>>(json);
+                ProcessItems = new ObservableCollection<ProcessItemInfo>(from x in processesSet
+                    select CreateProcessItemInfo(new ProcessItemInfo {FullPath = x}))
+                {
+                    CreateDummyProcessItemInfo()
+                };
+            }
+            else
+            {
+                ProcessItems = new ObservableCollection<ProcessItemInfo>
+                {
+                    CreateDummyProcessItemInfo()
+                };
             }
         }
 
         private ProcessItemInfo CreateProcessItemInfo(ProcessItemInfo processItemInfo)
         {
+            processItemInfo.FullPathChanged.Subscribe(tuple =>
+            {
+                if (tuple.prevType == ProcessItemType.Dummy)
+                    ProcessItems.Add(CreateDummyProcessItemInfo());
+                if (string.IsNullOrEmpty(tuple.fullPath))
+                {
+                    ProcessItems.Remove(tuple.sender);
+                }
+            });
+
+            processItemInfo.FullPathGotFocus.Subscribe(item =>
+            {
+                ProcessSelectedItem = item;
+            });
 
             return processItemInfo;
         }
 
+        private ProcessItemInfo CreateDummyProcessItemInfo()
+        {
+            return CreateProcessItemInfo(new ProcessItemInfo
+            {
+                Type = ProcessItemType.Dummy
+            });
+        }
+
+        public void MouseDownOutside(ProcessItemInfo item)
+        {
+            if (item == null)
+                return;
+
+            Keyboard.ClearFocus();
+        }
+
         public HashSet<string> Save()
         {
-            var lines = ProcessesText.Split('\n');
-            processesSet.Clear();
-            foreach (var line in lines)
-            {
-                if (!processesSet.Contains(line))
-                    processesSet.Add(line);
-            }
+            var processesSet = new HashSet<string>(from x in ProcessItems where !string.IsNullOrEmpty(x.FullPath) select x.FullPath);
 
             var json = JsonConvert.SerializeObject(processesSet);
             File.WriteAllText(jsonPath, json);
--- KeyConverter/KeyConverterGUI/ViewModels/ProcessSettingViewModel.cs
+++ KeyConverter/KeyConverterGUI/ViewModels/ProcessSettingViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using CommonStyleLib.Models;
 using CommonStyleLib.ViewModels;
 using CommonStyleLib.Views;
 using KeyConverterGUI.Models;
+using Prism.Commands;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
 
@@ -16,15 +18,36 @@ namespace KeyConverterGUI.ViewModels
     {
         public ProcessSettingViewModel(WindowService windowService, ProcessSettingModel model) : base(windowService, model)
         {
+            this.model = model;
+
             ProcessItems = model.ProcessItems.ToReadOnlyReactiveCollection(m => m);
-            ProcessesText = model.ToReactivePropertyAsSynchronized(m => m.ProcessesText);
+            ProcessSelectedItem = model.ObserveProperty(m => m.ProcessSelectedItem).ToReactiveProperty();
+
+            ProcessItemsMouseDownCommand = new DelegateCommand<ProcessItemInfo>(ProcessItemsMouseDown);
         }
+
+        #region Fields
+
+        private readonly ProcessSettingModel model;
+
+        #endregion
 
         #region Properties
 
         public ReadOnlyReactiveCollection<ProcessItemInfo> ProcessItems { get; set; }
-        public ReactiveProperty<string> ProcessesText { get; set; }
+        public ReactiveProperty<ProcessItemInfo> ProcessSelectedItem { get; set; }
 
         #endregion
+
+        #region Event Properties
+
+        public ICommand ProcessItemsMouseDownCommand { get; set; }
+
+        #endregion
+
+        public void ProcessItemsMouseDown(ProcessItemInfo item)
+        {
+            model.MouseDownOutside(item);
+        }
     }
 }
