c68fe0c97e6cf941db5bf926de5c7a7423108f0f
AonaSuzutsuki <rvvfake.org@gmail.com>
2020-10-08 12:42:11 +0900

    change title.

--- KeyConverter/KeyConverterGUI/Properties/AssemblyInfo.cs
+++ KeyConverter/KeyConverterGUI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("MabinogiKeyConverter")]
+[assembly: AssemblyTitle("KimamaKeyConverter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MabinogiKeyConverter")]
+[assembly: AssemblyProduct("KimamaKeyConverter")]
 [assembly: AssemblyCopyright("Copyright © Aona Suzutsuki  2018 - 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
