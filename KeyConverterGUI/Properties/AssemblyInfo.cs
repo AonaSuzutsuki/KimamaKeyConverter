64b1089c986c0b95a4f92e9b39df614719aa89b5
AonaSuzutsuki <rvvfake.org@gmail.com>
2021-08-18 10:33:00 +0900

    fixed version.

--- KeyConverterGUI/Properties/AssemblyInfo.cs
+++ KeyConverterGUI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KimamaKeyConverter")]
-[assembly: AssemblyCopyright("Copyright © Aona Suzutsuki  2018 - 2020")]
+[assembly: AssemblyCopyright("Copyright © Aona Suzutsuki  2018 - 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.8.6")]
-[assembly: AssemblyFileVersion("1.1.8.6")]
+[assembly: AssemblyVersion("1.1.9.7")]
+[assembly: AssemblyFileVersion("1.1.9.7")]
